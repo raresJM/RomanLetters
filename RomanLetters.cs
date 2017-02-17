bdc5d84f001ff2b7339c4a22ffd65a11a273b2f5
raresJM <rares.scarlat.jm@gmail.com>
2017-02-17 02:13:36 +0200

    added test methods

--- RomanLetters.cs
+++ RomanLetters.cs
@@ -6,9 +6,67 @@ namespace RomanLetters
     [TestClass]
     public class RomanLetters
     {
+
         [TestMethod]
-        public void TestMethod1()
+        public void TestNumberLowerThanFour()
         {
+            Assert.AreEqual("III", convertNumber(3));
         }
+        [TestMethod]
+        public void TestNumberFour()
+        {
+            Assert.AreEqual("IV", convertNumber(4));
+        }
+        [TestMethod]
+        public void TestNumberEight()
+        {
+            Assert.AreEqual("VIII", convertNumber(8));
+        }
+        [TestMethod]
+        public void TestNumberNine()
+        {
+            Assert.AreEqual("IX", convertNumber(9));
+        }
+        [TestMethod]
+        public void TestNumberFifteen()
+        {
+            Assert.AreEqual("XV", convertNumber(15));
+        }
+        [TestMethod]
+        public void TestNumberThirty()
+        {
+            Assert.AreEqual("XXX", convertNumber(30));
+        }
+        [TestMethod]
+        public void TestNumberThirtyNine()
+        {
+            Assert.AreEqual("XXXIX", convertNumber(39));
+        }
+        [TestMethod]
+        public void TestNumberFortyOne()
+        {
+            Assert.AreEqual("XLI", convertNumber(41));
+        }
+        [TestMethod]
+        public void TestNumberFortyFive()
+        {
+            Assert.AreEqual("XLV", convertNumber(45));
+        }
+        [TestMethod]
+        public void TestNumberNinetyNine()
+        {
+            Assert.AreEqual("XCIX", convertNumber(99));
+        }
+        [TestMethod]
+        public void TestNumberOneHundred()
+        {
+            Assert.AreEqual("C", convertNumber(100));
+        }
+        [TestMethod]
+        public void TestNumberOutOfRange()
+        {
+            Assert.AreEqual("ERROR!", convertNumber(101));
+        }
+
     }
 }
