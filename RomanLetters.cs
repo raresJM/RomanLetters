e17c1d7d7e6b5679019d3d117dca0e43c92b8b98
raresJM <rares.scarlat.jm@gmail.com>
2017-02-17 02:01:00 +0200

    created the project RomanLetters

---
+++ RomanLetters.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RomanLetters
+{
+    [TestClass]
+    public class RomanLetters
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
