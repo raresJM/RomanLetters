5d58a8cdfed607de323026b08fc5b2c412084315
raresJM <rares.scarlat.jm@gmail.com>
2017-02-20 22:26:03 +0200

    refactoring the Roman Letters problems - 2

--- RomanLetters.cs
+++ RomanLetters.cs
@@ -68,75 +68,88 @@ namespace RomanLetters
             Assert.AreEqual("ERROR!", convertNumber(101));
         }
         [TestMethod]
-        public void TestReturnRomanValue()
+        public void TestReturnRomanValue_01()
         {
             String[] romanDigits = { "I", "IV", "V", "IX" };
             String[] romanTens = { "X", "XL", "L", "XC" };
-            Assert.AreEqual("I", returnRomanValue(1,romanDigits,))
+            int[] arabicDigits = { 1, 4, 5, 9 };
+            int[] arabicTens = { 10, 40, 50, 90 };
+            Assert.AreEqual("I", returnRomanValue(1, romanDigits, arabicDigits));
         }
-
-
-
-        public String convertTensToRoman(int tens)
+        [TestMethod]
+        public void TestReturnRomanValue_02()
         {
-            String result = "";
-            if (tens < 100 && tens >= 90) 
-            {
-                result = "XC";
-            }else
-                if (tens < 90 && tens >= 80)
-                {
-                    result = "LXXX";
-                }else
-                    if (tens < 80 && tens >= 70)
-                    {
-                        result = "LXX";
-                    }else
-                        if (tens < 70 && tens >= 60)
-                        {
-                            result = "LX";
-                        }else
-                            if (tens < 60 && tens >= 50)
-                            {
-                                result = "L";
-                            }else
-                                if (tens < 50 && tens >= 40)
-                                {
-                                    result = "XL";
-                                }else
-                                    if (tens < 40 && tens >= 30)
-                                    {
-                                        result = "XXX";
-                                    }else
-                                        if (tens < 30 && tens >= 20)
-                                        {
-                                            result = "XX";
-                                        }else
-                                            if ((tens < 20) && (tens >= 10))
-                                            {
-                                              
-                                                result = "X";
-                                            }
-            return result;
-        
+            String[] romanDigits = { "I", "IV", "V", "IX" };
+            String[] romanTens = { "X", "XL", "L", "XC" };
+            int[] arabicDigits = { 1, 4, 5, 9 };
+            int[] arabicTens = { 10, 40, 50, 90 };
+            Assert.AreEqual("X", returnRomanValue(10, romanTens, arabicTens));
         }
 
-        //method to return a String value representing the Roman representation of the 
+
+
+        //public String convertTensToRoman(int tens)
+        //{
+        //    String result = "";
+        //    if (tens < 100 && tens >= 90) 
+        //    {
+        //        result = "XC";
+        //    }else
+        //        if (tens < 90 && tens >= 80)
+        //        {
+        //            result = "LXXX";
+        //        }else
+        //            if (tens < 80 && tens >= 70)
+        //            {
+        //                result = "LXX";
+        //            }else
+        //                if (tens < 70 && tens >= 60)
+        //                {
+        //                    result = "LX";
+        //                }else
+        //                    if (tens < 60 && tens >= 50)
+        //                    {
+        //                        result = "L";
+        //                    }else
+        //                        if (tens < 50 && tens >= 40)
+        //                        {
+        //                            result = "XL";
+        //                        }else
+        //                            if (tens < 40 && tens >= 30)
+        //                            {
+        //                                result = "XXX";
+        //                            }else
+        //                                if (tens < 30 && tens >= 20)
+        //                                {
+        //                                    result = "XX";
+        //                                }else
+        //                                    if ((tens < 20) && (tens >= 10))
+        //                                    {
+                                              
+        //                                        result = "X";
+        //                                    }
+        //    return result;
+        
+        //}
+
+        //method to return a String value representing the Roman 
+        //representation of the 
         //parameter digit
-        public String returnRomanValue(int digit, String[] romanValues, int[] arabicValues)
+        public String returnRomanValue(int digit, String[] roman, int[] arabic)
         {
             String result = "";
             while (digit > 0)
             {
-                for (int i = arabicValues.Length - 1; i > 0; i--)
+                for (int i = arabic.Length - 1; i > 0; i--)
                 {
-                    if (digit == arabicValues[i])
+                    if (digit == arabic[i])
                     {
-                        digit -= arabicValues[i];
-                        result += romanValues[i];
+                        digit -= arabic[i];
+                        result += roman[i];
                     }   
                 }
             }
+            return result;
         }
 
 
@@ -147,7 +160,7 @@ namespace RomanLetters
             String result = "";
             String [] romanDigits = {"I", "IV", "V", "IX"};
             String[] romanTens = { "X", "XL", "L", "XC" };
-            int[] arabicDigits = { 1, 4, 5, 9 };
+            int[] arabicDigits = {1, 4, 5, 9};
             int[] arabicTens = {10,40,50,90};
 
             return result;
